4fbd35331f6769f23d447695a132d4e63d76c98e
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-11 17:10:03 +0200

    Receive answer on back end, change the score and manipulate game status

--- routes/round.js
+++ routes/round.js
@@ -4,6 +4,7 @@ function roundRoute(app) {
     let score = 0;
     //let callAFriendUsed, askTheCrowdUsed, halfBankUsed = false;
     let questions;
+    let isGameOver = false;
     
     fs.readFile('./data/questions.json', (err, content) => {
         if (err) throw err;
@@ -17,6 +18,10 @@ function roundRoute(app) {
             res.json({
                 winner: true,
             });
+        } else if (isGameOver) {
+            res.json({
+                loser: true,
+            });
         } else {
             const nextQuestion = questions[score];
             const { question } = nextQuestion;
@@ -27,7 +32,30 @@ function roundRoute(app) {
                 question, answers
             });
         }
-    });    
+    });
+
+    app.post('/answer/:index', (req, res) => {
+        if (isGameOver) {
+            res.json({
+                loser: true,
+            });
+        };
+
+        const { index } = req.params;
+        const question = questions[score];
+        const isCorrect = (question.answers[index].isCorrect === 'true');
+
+        if (isCorrect) {
+            score++;
+        } else {
+            isGameOver = true;
+        }
+
+        res.json({
+            correct: isCorrect,
+            score
+        });
+    })
 }
 
 module.exports = roundRoute;
